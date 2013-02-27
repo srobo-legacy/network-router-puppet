832f327d5af915962bfe905573d2bba16ac15af6
Chris Malton <chrism@cmalton.me.uk>
2013-02-27 21:48:08 +0000

    Add scripts to initialise DNS.

--- modules/sr-router/manifests/bind.pp
+++ modules/sr-router/manifests/bind.pp
@@ -10,6 +10,22 @@ class sr-router::bind {
     notify => Service["bind9"],
   }
 
+  file { "/usr/local/sbin/dns-init":
+    ensure => "present",
+    source => "puppet:///modules/sr-router/dns-init",
+    require => Package["bind9"],
+    mode => 700,
+    owner => 'root',
+    group => 'root',
+  }
+
+  exec { "/usr/local/sbin/dns-init":
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    creates => "/etc/bind/db.net.studentrobotics.org",
+    notify => Service["bind9"],
+    require => Group["sr-dnsadmins"],
+  }
+
   service { "bind9":
     ensure => "running",
     enable => true,
