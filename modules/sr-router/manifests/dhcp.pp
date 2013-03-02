b5bd9d483be14c826f57683ddf42d6c27befe2ea
Chris Malton <chrism@cmalton.me.uk>
2013-03-02 23:32:10 +0000

    Add scripts to initialise the DHCP server.

--- modules/sr-router/manifests/dhcp.pp
+++ modules/sr-router/manifests/dhcp.pp
@@ -3,6 +3,22 @@ class sr-router::dhcp {
     ensure => "present",
   }
 
+  file { "/usr/local/sbin/dhcp-init":
+    ensure => "present",
+    source => "puppet:///modules/sr-router/dhcp-init",
+    require => Package["isc-dhcp-server"],
+    mode => 700,
+    owner => 'root',
+    group => 'root',
+  }
+
+  exec { "/usr/local/sbin/dns-init 30":
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    creates => "/etc/dhcp/sr-team-subnets.conf",
+    notify => Service["isc-dhcp-server"],
+    require => [ File["/usr/local/sbin/dhcp-init"], Group["sr-dhcpadmins"] ],
+  }
+
   file { "/etc/dhcp/dhcpd.conf":
     ensure => "present",
     source => "puppet:///modules/sr-router/dhcpd.conf",
