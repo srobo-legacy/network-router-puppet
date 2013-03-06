ab43378a4ed30839b514c76845cc6e9a05a2f2b9
Chris Malton <chrism@cmalton.me.uk>
2013-03-06 12:05:30 +0000

    Add MRTG configuration magic.

---
+++ modules/sr-router/manifests/mrtg.pp
@@ -0,0 +1,42 @@
+class sr-router::mrtg {
+
+  package{ ["mrtg", "snmp", "snmpd"]:
+    ensure => "latest",
+  }
+
+  file{ "/etc/mrtg/":
+    ensure => "directory",
+    mode => "755",
+    owner => "root",
+    group => "root",
+  }
+  
+  file{ "/etc/mrtg/conf.d":
+    ensure => "directory",
+    mode => "755",
+    owner => "root",
+    group => "root",
+    require => File["/etc/mrtg/"],
+  }
+  
+  file{ "/var/www/":
+    ensure => "directory",
+    mode => "755",
+    owner => "root",
+    group => "root",
+    require => Package["apache2"],
+  }
+  
+  file{ "/usr/local/bin/create_mrtg_config":
+    ensure => "present",
+    mode => "744",
+    owner => "root",
+    group => "root",
+    source => "puppet:///modules/sr-router/create_mrtg_config",
+  }
+  
+  exec{ "/usr/local/bin/create_mrtg_config":
+    }
+  
+    
+}
