b9cd57f8c6553986b5ff64c6620ca1b852be78b0
Chris Malton <chrism@cmalton.me.uk>
2013-03-15 15:04:05 +0000

    Add the second ntop config file (apparently, 1 is not enough!)

--- modules/sr-router/manifests/ntop.pp
+++ modules/sr-router/manifests/ntop.pp
@@ -5,11 +5,20 @@ class sr-router::ntop {
 
     service { "ntop":
       ensure => "running",
-      require => [ Package["ntop"], File["/etc/default/ntop"] ],
+      require => [ Package["ntop"], File["/etc/default/ntop"], File["/var/lib/ntop/init.cfg"] ],
     }
 
     file { "/etc/default/ntop":
       source => "puppet:///modules/sr-router/ntop.default",
+      owner => "root",
+      group => "root",
+      mode => "644",
+      require => Package["ntop"],
+      notify => Service["ntop"],
+    }
+
+    file { "/var/lib/ntop/init.cfg":
+      source => "puppet:///modules/sr-router/ntop.cfg",
       owner => "root",
       group => "root",
       mode => "644",
