d8e01abe2e1fcdd6db86be06d308cd04070b52aa
Chris Malton <chrism@cmalton.me.uk>
2013-03-15 15:16:05 +0000

    Add mrtg init script.

--- modules/sr-router/manifests/mrtg.pp
+++ modules/sr-router/manifests/mrtg.pp
@@ -19,12 +19,48 @@ class sr-router::mrtg {
     require => File["/etc/mrtg/"],
   }
   
-  file{ "/var/www/":
+  file{ "/var/www/mrtg":
     ensure => "directory",
     mode => "755",
     owner => "root",
     group => "root",
     require => Package["apache2"],
+  }
+
+  # MRTG init script
+  file { '/etc/init.d/mrtg':
+    ensure => present,
+    owner => 'root',
+    group => 'root',
+    mode => '755',
+    source => 'puppet:///modules/sr-router/mrtg.init',
+    before => Service['mrtg'],
+  }
+
+  # MRTG service
+  service { 'mrtg':
+    ensure      => running,
+    enable      => true,
+    hasrestart  => true,
+    hasstatus   => false,
+  }
+
+  exec { "/usr/sbin/a2enmod rewrite":
+    creates => "/etc/apache2/mods-enabled/rewrite.load"
+  }
+
+  exec { "/usr/sbin/a2ensite mrtg.net.studentrobotics.org":
+    creates => "/etc/apache2/sites-enabled/mrtg.net.studentrobotics.org"
+    require => File["/etc/apache2/sites-available/mrtg.net.studentrobotics.org"],
+  }
+
+  file { "/etc/apache2/sites-available/mrtg.net.studentrobotics.org":
+    ensure => "present",
+    source => "puppet:///modules/sr-router/mrtg.apache2.conf",
+    mode => "644",
+    owner => "root",
+    group => "root",
+    require => Exec["/usr/sbin/a2enmod rewrite"],
   }
   
   file{ "/usr/local/bin/create_mrtg_config":
